553f2e29f6bc08a20f14d5a5963a2a21d8ab0b58
Katya Eritano <keritano@stanford.edu>
2020-01-27 15:49:17 -0800

    automated commit

--- scripts/failed_banks_ca.sh
+++ scripts/failed_banks_ca.sh
@@ -2,6 +2,6 @@ mkdir Assignment_1
 cd Assignment_1
 wget https://www.fdic.gov/bank/individual/failed/banklist.csv
 touch failed_banks_ca.csv
-head -n 1 /home/keritano/Desktop/Assignment_1/banklist.csv > failed_banks_ca.csv
-grep CA /home/keritano/Desktop/Assignment_1/banklist.csv >> failed_banks_ca.csv
-touch assm1.py
+head -n 1 banklist.csv > failed_banks_ca.csv
+grep CA banklist.csv >> failed_banks_ca.csv
+wc -l failed_banks_ca.csv
