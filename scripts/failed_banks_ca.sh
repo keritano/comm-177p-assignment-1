470bac12d5c20ec6581c1d22391a2355b05e46e1
Katya Eritano <keritano@stanford.edu>
2020-01-27 18:33:16 -0800

    automated commit

--- scripts/failed_banks_ca.sh
+++ scripts/failed_banks_ca.sh
@@ -1,7 +1,12 @@
-mkdir Assignment_1
-cd Assignment_1
 wget https://www.fdic.gov/bank/individual/failed/banklist.csv
 touch failed_banks_ca.csv
 head -n 1 banklist.csv > failed_banks_ca.csv
 grep CA banklist.csv >> failed_banks_ca.csv
-wc -l failed_banks_ca.csv
+
+#!/bin/bash
+bnkcnt=0
+bnkcnt=$(cat failed_banks_ca.csv| wc -l)
+bnkcnt=$(($bnkcnt -1))
+
+STR="There are $bnkcnt failed banks in CA."
+echo $STR
